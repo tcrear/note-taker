8eb5e18c04cb5851935067b57e9e3a595466e369
tcrear <smileybear88@hotmail.com>
2021-11-22 15:51:06 -0600

    routes fixed, server saved

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
+const routes = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,23 +11,10 @@ const app = express();
 app.use(clog);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
 
 app.use(express.static('public'));
+app.use('/', routes); 
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
-);
-
-
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
-);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
